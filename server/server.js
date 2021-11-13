33a5bcefd88f7f1b1ba89d29a5884aa64f3e954d
Tobi Oyeleye <krisella74@gmail.com>
2021-11-13 02:15:04 +0100

    start server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,24 @@
-const express = require('express')
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 
 app = express();
 
-app.get('/', (req, res) => {
-    res.json('OK')
+const MONGO_URI =
+  "mongodb://127.0.0.1:27017/productfeedbackdb?retryWrites=true&w=majority";
+
+mongoose.connect(MONGO_URI)
+    .then(() => {console.log('db connected')})
+    .catch(() => {console.log('error connecting db')})
+
+app.get('/healthcheck', (req, res) => {
+    res.json('EVERYTHING SEEM FINE')
 })
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema
+}))
 
 app.listen(3000, () => console.log('app is running on port 3000'))
